4f447ebd98c88fec5228c833325de245ec140ce9
Marcos França <marcosfranca310430@gmail.com>
2024-01-05 13:15:07 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var nome = 'Marcos';
+var idade = 23;
+var facul = true; 
+var comidaFavorita = 'Macarrao';
+var time = null;
+var sobrenome = 'Fraça';
+
+console.log (nome, idade, facul, comidaFavorita)
+console.log (typeof nome )
+console.log (typeof nome )
+console.log (nome + ' ' + sobrenome  )
