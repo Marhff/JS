c68bc2859ad4ce6914e4de543952848131fe0e74
Marcos França <marcosfranca310430@gmail.com>
2024-01-05 14:12:23 -0300

    Inicial

---
+++ 0201-variaveis/script-aula.js
@@ -0,0 +1,30 @@
+var nome = 'Marcos';
+var idade = 23;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade, nome);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = 'França',
+    cidade = 'BH';
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+// console.log(aindaNaoDefine);
+var comida;
+console.log(comida);
+comida = 'Pizza';
+console.log(comida);
+
+
+
+// console.log(time);
+
---
+++ 0201-variaveis/script.js
@@ -0,0 +1,21 @@
+// Declarar uma variável com o seu nome
+var nome = 'Marcos';
+
+// Declarar uma variável com a sua idade
+var idade = 23;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'Pizza';
+
+// Declarar 5 variáveis diferentes sem valores
+var time = 'Cruzeiro',
+  Empreendedor,
+  pais,
+  endereco,
+  rua;
+
+console.log(nome, idade, comidaFavorita);
